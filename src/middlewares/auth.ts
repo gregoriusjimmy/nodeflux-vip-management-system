8555f2afc95485d73b2d2341c7e1a58036988a22
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-07-15 17:45:00 +0700

    add simple authentication and use it in vips route

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express'
+
+const SECRET_KEY: string = 'stafftoken'
+const ACCOUNT_NAME: string = 'staffusername'
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const username = req.query.username
+  const password = req.query.password
+  if (username && password) {
+    if (username !== ACCOUNT_NAME && password !== SECRET_KEY) {
+      return res.status(400).json('authentication failed')
+    } else next()
+  } else {
+    return res.status(400).json('not verified')
+  }
+}
