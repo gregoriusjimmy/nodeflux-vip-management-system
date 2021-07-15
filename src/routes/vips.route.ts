7317474cc590cd122cdcf0494a8f10854b0a15f4
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-07-15 18:35:12 +0700

    fix router parameter for http patch and remove vip route parameter

--- src/routes/vips.route.ts
+++ src/routes/vips.route.ts
@@ -1,17 +1,18 @@
-import { Sequelize } from 'sequelize'
 import { Router } from 'express'
 import {
   handleVipsGet,
   handleVipsPost,
-  handleVipsPatch,
+  handleVipsPatchArrived,
 } from '../controllers/vips.controller'
 
-const vipsRoute = (db: Sequelize) => {
+const vipsRoute = () => {
   const router = Router()
 
-  router.get('/', (req, res) => handleVipsGet(req, res, db))
-  router.post('/', (req, res) => handleVipsPost(req, res, db))
-  router.patch('/', (req, res) => handleVipsPatch(req, res, db))
+  router.get('/', (req, res) => handleVipsGet(req, res))
+  // router.get('/:id', (req, res) => handleVipsGetById(req, res))
+  router.post('/', (req, res) => handleVipsPost(req, res))
+  // router.put('/:id', (req, res) => handleVipsPost(req, res, db))
+  router.patch('/:id/arrived', (req, res) => handleVipsPatchArrived(req, res))
 
   return router
 }
