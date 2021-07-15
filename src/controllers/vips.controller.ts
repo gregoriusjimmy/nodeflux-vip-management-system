74b88f3f3d00e3011107c5b96315214564fb6d26
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-07-15 18:37:22 +0700

    modify all handler to use async await method,  fix handle patch arrived to use id from url params

--- src/controllers/vips.controller.ts
+++ src/controllers/vips.controller.ts
@@ -1,35 +1,39 @@
 import { createResponseBody } from './../utilities/createResponseBody'
-import { Sequelize } from 'sequelize'
 import { Request, Response } from 'express'
 import { VIP } from '../models/vip'
 
-export const handleVipsGet = (req: Request, res: Response, db: Sequelize) => {
-  VIP.findAll()
-    .then((vips) => {
-      res
-        .status(200)
-        .json(createResponseBody({ ok: true, data: vips, message: 'success' }))
-    })
-    .catch((err) => {
-      console.log(err)
-      res.status(400).json(
-        createResponseBody({
-          ok: false,
-          message: 'failed to get vips, reason: ' + err,
-        })
-      )
-    })
+export const handleVipsGet = async (req: Request, res: Response) => {
+  try {
+    const vips = await VIP.findAll()
+    res
+      .status(200)
+      .json(createResponseBody({ ok: true, data: vips, message: 'success' }))
+  } catch (err) {
+    console.log(err)
+    res.status(400).json(
+      createResponseBody({
+        ok: false,
+        message: 'failed to get vips, reason: ' + err,
+      })
+    )
+  }
 }
 
-export const handleVipsPost = (req: Request, res: Response, db: Sequelize) => {
+export const handleVipsPost = async (req: Request, res: Response) => {
   try {
-    const { name, country_of_origin, eta, photo, attributes } = req.body
-    VIP.create({ name, country_of_origin, eta, photo, attributes }).then(
-      (vip) =>
-        res
-          .status(200)
-          .json(createResponseBody({ ok: true, message: 'success adding vip' }))
-    )
+    const { name, country_of_origin, eta, arrived, photo, attributes } =
+      req.body
+    await VIP.create({
+      name,
+      country_of_origin,
+      arrived,
+      eta,
+      photo,
+      attributes,
+    })
+    res
+      .status(200)
+      .json(createResponseBody({ ok: true, message: 'success adding vip' }))
   } catch (err) {
     console.log('error occurred: ' + err)
     res.status(400).json(
@@ -41,21 +45,21 @@ export const handleVipsPost = (req: Request, res: Response, db: Sequelize) => {
   }
 }
 
-export const handleVipsPatch = (req: Request, res: Response, db: Sequelize) => {
+export const handleVipsPatchArrived = async (req: Request, res: Response) => {
   try {
-    const { id, arrived } = req.body
-    VIP.update(
+    const id = req.params.id
+    const { arrived } = req.body
+    await VIP.update(
       { arrived },
       {
         where: {
           id: id,
         },
       }
-    ).then((vip) =>
-      res
-        .status(200)
-        .json(createResponseBody({ ok: true, message: 'success patching vip' }))
     )
+    res
+      .status(200)
+      .json(createResponseBody({ ok: true, message: 'success patching vip' }))
   } catch (err) {
     console.log('error occurred: ' + err)
     res.status(400).json(
