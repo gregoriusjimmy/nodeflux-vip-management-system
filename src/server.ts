adb3cb379c246dc659cbf321248787ea5b226087
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-07-15 18:38:37 +0700

    add express json and urlencoded in server to able receive form input

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import { db } from './config/database'
 import vipsRoute from './routes/vips.route'
 const PORT = 3000
 const app: Application = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 db.authenticate()
   .then(async () => {
@@ -15,6 +17,6 @@ db.authenticate()
   })
   .catch((err) => console.error('Unable to connect to the database:', err))
 
-app.use('/api/vips', requireAuth, vipsRoute(db))
+app.use('/api/vips', requireAuth, vipsRoute())
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
