17d8be8deb280fc4afec285566a9c39fd6ad19a2
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-07-15 19:12:25 +0700

    add put and get by id handler, refactor console.log to console.error, fix wrong message's respond in vips controller

--- src/controllers/vips.controller.ts
+++ src/controllers/vips.controller.ts
@@ -9,7 +9,25 @@ export const handleVipsGet = async (req: Request, res: Response) => {
       .status(200)
       .json(createResponseBody({ ok: true, data: vips, message: 'success' }))
   } catch (err) {
-    console.log(err)
+    console.error(err)
+    res.status(400).json(
+      createResponseBody({
+        ok: false,
+        message: 'failed to get vips, reason: ' + err,
+      })
+    )
+  }
+}
+
+export const handleVipsGetById = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+    const vip = await VIP.findAll({ where: { id: id } })
+    res
+      .status(200)
+      .json(createResponseBody({ ok: true, data: vip, message: 'success' }))
+  } catch (err) {
+    console.error(err)
     res.status(400).json(
       createResponseBody({
         ok: false,
@@ -26,8 +44,8 @@ export const handleVipsPost = async (req: Request, res: Response) => {
     await VIP.create({
       name,
       country_of_origin,
-      arrived,
       eta,
+      arrived,
       photo,
       attributes,
     })
@@ -35,7 +53,7 @@ export const handleVipsPost = async (req: Request, res: Response) => {
       .status(200)
       .json(createResponseBody({ ok: true, message: 'success adding vip' }))
   } catch (err) {
-    console.log('error occurred: ' + err)
+    console.error(err)
     res.status(400).json(
       createResponseBody({
         ok: false,
@@ -45,27 +63,51 @@ export const handleVipsPost = async (req: Request, res: Response) => {
   }
 }
 
+export const handleVipsPut = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+    const { name, country_of_origin, eta, arrived, photo, attributes } =
+      req.body
+    await VIP.update(
+      {
+        name,
+        country_of_origin,
+        arrived,
+        eta,
+        photo,
+        attributes,
+      },
+      { where: { id: id } }
+    )
+    res
+      .status(200)
+      .json(createResponseBody({ ok: true, message: 'success modifying vip' }))
+  } catch (err) {
+    console.error(err)
+    res.status(400).json(
+      createResponseBody({
+        ok: false,
+        message: 'failed to modify vip, reason: ' + err,
+      })
+    )
+  }
+}
+
 export const handleVipsPatchArrived = async (req: Request, res: Response) => {
   try {
     const id = req.params.id
     const { arrived } = req.body
-    await VIP.update(
-      { arrived },
-      {
-        where: {
-          id: id,
-        },
-      }
-    )
+    if (!arrived) throw Error('arrived cannot be empty')
+    await VIP.update({ arrived: arrived }, { where: { id: id } })
     res
       .status(200)
       .json(createResponseBody({ ok: true, message: 'success patching vip' }))
   } catch (err) {
-    console.log('error occurred: ' + err)
+    console.error(err)
     res.status(400).json(
       createResponseBody({
         ok: false,
-        message: 'failed to add vip, reason: ' + err,
+        message: 'failed to patch vip, reason: ' + err,
       })
     )
   }
