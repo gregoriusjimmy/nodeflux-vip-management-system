0527a2b1f5a6499135b7b165f6a650308a138ffb
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-07-18 09:39:31 +0700

    add styling to the home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import { API_URL } from '../constants'
-import { TextField, Button } from '@material-ui/core'
+import { TextField, Button, Typography } from '@material-ui/core'
 import Layout from '../components/Layout'
-
+import styles from '../styles/Home.module.css'
 export default function Home() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -23,38 +23,49 @@ export default function Home() {
           router.push('/vips')
         }
       })
-      .catch((e) => console.log(e))
+      .catch((e) => console.error(e))
   }
 
   return (
     <Layout>
-      <div>
-        <form>
-          <TextField
-            id='username'
-            name='username'
-            label='Username'
-            variant='outlined'
-            fullWidth
-            required
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            id='password'
-            email='password'
-            type='password'
-            label='Password'
-            variant='outlined'
-            fullWidth
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button variant='contained' color='primary' onClick={handleSubmit}>
-            Login
-          </Button>
-        </form>
+      <div className={styles.container}>
+        <div>
+          <Typography align='center' variant='h2' component='h2' gutterBottom>
+            Vip Management System <br /> <strong>Nodeflux</strong>
+          </Typography>
+        </div>
+        <div>
+          <form className={styles.formContainer}>
+            <TextField
+              id='username'
+              name='username'
+              label='Username'
+              variant='outlined'
+              fullWidth
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              className={styles.inputField}
+              id='password'
+              email='password'
+              type='password'
+              label='Password'
+              variant='outlined'
+              fullWidth
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              fullWidth
+              variant='contained'
+              color='primary'
+              onClick={handleSubmit}
+            >
+              Login
+            </Button>
+          </form>
+        </div>
       </div>
     </Layout>
   )
