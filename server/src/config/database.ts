fc17994cdf282f8d8009c4053752bf6f6d96ac59
sonadztux <dev.sonadztux@gmail.com>
2021-09-10 00:23:06 +0700

    fix host & cors issues when running without docker

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from 'sequelize'
 
 export const db = new Sequelize(
-  process.env.PG_DATABASE || 'postgres',
+  process.env.PG_DATABASE || 'vip_management_system',
   process.env.PG_USER || 'postgres',
-  process.env.PG_PASSWORD || '',
+  process.env.PG_PASSWORD || ' ',
   {
     host: process.env.PG_HOST || 'localhost',
     port: process.env.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
